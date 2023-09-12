816a294af229e10d04b2f5fae669c92dbc9de423
valen <valenemartini@gmail.com>
2023-09-12 19:24:36 -0300

    correccion distribucion normal

--- src/components/Procesos/CalculoDistribuciones/calculoNormal.js
+++ src/components/Procesos/CalculoDistribuciones/calculoNormal.js
@@ -1,10 +1,20 @@
 export const generarDistribucionNormal = (muestra, media, desviacion) => {
   const distribucion = [];
-  for (let i = 0; i < muestra; i++) {
+  for (let i = 0; i < (muestra / 2).toFixed(0); i++) {
+    const rnd1 = Math.random();
+    const rnd2 = Math.random();
     distribucion.push(
       Number(
-        Math.sqrt(-2 * Math.log(Math.random())) *
-          Math.cos(2 * Math.PI * Math.random()) *
+        Math.sqrt(-2 * Math.log(1 - rnd1)) *
+          Math.cos(2 * Math.PI * rnd2) *
+          Number(desviacion) +
+          Number(media)
+      )
+    );
+    distribucion.push(
+      Number(
+        Math.sqrt(-2 * Math.log(1 - rnd1)) *
+          Math.sin(2 * Math.PI * rnd2) *
           Number(desviacion) +
           Number(media)
       )
